2725acf39e8cbefb1dfbd10024de664ef957cdba
DeadCloud <pavel2012ig@gmail.com>
2023-01-28 22:07:26 +0300

    added popup

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,5 @@
+// maps par
+
 ymaps.ready(function () {
     var map = new ymaps.Map('map', {
         center: [59.934390, 30.334484],
@@ -26,28 +28,10 @@ ymaps.ready(function () {
     map.geoObjects.add(squarePlacemark);
 
 });
-// var menu = ['Slide 1', 'Slide 2', 'Slide 3', 'Slide 4'];
 
-// var swiper = new Swiper(".mySwiper", {
-//     scrollbar: {
-//         el: ".swiper-scrollbar"
+// maps par
 
-//     },
-//     pagination: {
-//         el: value.closest('.slider__wrapper').querySelector(".swiper-pagination"),
-//         clickable: true,
-//         // renderBullet: function (index, className) {
-//         //   return '<span class="' + className + '"> ' + ((menu[index])+ '/' + menu.length)  + '</span>';
-//         // }
-//     },
-
-//     autoplay: {
-//         delay: 4000,
-//         disableOnInteraction: false
-//     }
-
-// }
-// );
+// swiper par
 
 let swipers = document.querySelectorAll(".swiper1")
 swipers.forEach(value => {
@@ -96,7 +80,7 @@ var swiper2 = new Swiper(".swiper2", {
     watchSlidesProgress: true,
     slidesPerView: 2.5,
     centeredSlides: true,
-    centerInsufficientSlides:false,
+    centerInsufficientSlides: false,
     scrollbar: {
         el: ".swiper-scrollbar"
     },
@@ -109,3 +93,73 @@ var swiper2 = new Swiper(".swiper2", {
         prevEl: ".swiper-button-prev",
     }
 });
+
+// swiper par
+
+// popup par
+
+let popupBg = document.querySelector('.popup__bg'); // Фон попап окна
+let popup = document.querySelector('.popup'); // Само окно
+let popupSuc = document.querySelector('.popup_suc'); // Окно успешно!
+let openPopupButtons = document.querySelectorAll('.open-popup'); // Кнопки для показа окна
+let openSucButtons = document.querySelector('.send'); // Кнопки для показа окна
+let closePopupButton = document.querySelector('.close-popup'); // Кнопка для скрытия окна
+let closeSucButton = document.querySelector('.close-popup2'); // Кнопка для скрытия окна
+let closeSucButton2 = document.getElementById("close"); // Кнопка для скрытия окна
+
+openPopupButtons.forEach((button) => { // Перебираем все кнопки
+    button.addEventListener('click', (e) => { // Для каждой вешаем обработчик событий на клик
+        e.preventDefault(); // Предотвращаем дефолтное поведение браузера
+        popupBg.classList.add('active'); // Добавляем класс 'active' для фона
+        popup.classList.add('active'); // И для самого окна
+        const body = document.body;
+        body.style.height = '100vh';
+        body.style.overflowY = 'hidden';
+    })
+});
+
+closePopupButton.addEventListener('click', () => { // Вешаем обработчик на крестик
+    popupBg.classList.remove('active'); // Убираем активный класс с фона
+    popup.classList.remove('active'); // И с окна
+    popupSuc.classList.remove('active');
+    const body = document.body;
+    body.style.height = '';
+    body.style.overflowY = '';
+});
+closeSucButton2.addEventListener('click', () => { // Вешаем обработчик на крестик
+    popupBg.classList.remove('active'); // Убираем активный класс с фона
+    popupSuc.classList.remove('active');
+    const body = document.body;
+    body.style.height = '';
+    body.style.overflowY = '';
+});
+
+
+openSucButtons.addEventListener('click', (e) => {
+    e.preventDefault();
+    popupSuc.classList.add('active');
+    popup.classList.remove('active');
+
+});
+closeSucButton.addEventListener('click', () => {
+    popupBg.classList.remove('active'); // Убираем активный класс с фона
+    popup.classList.remove('active');
+    popupSuc.classList.remove('active');
+    const body = document.body;
+    body.style.height = '';
+    body.style.overflowY = '';
+});
+
+document.addEventListener('click', (e) => { // Вешаем обработчик на весь документ
+    if (e.target === popupBg) { // Если цель клика - фот, то:
+        popupBg.classList.remove('active'); // Убираем активный класс с фона
+        popup.classList.remove('active'); // И с окна
+        popupSuc.classList.remove('active');
+        const body = document.body;
+        body.style.height = '';
+        body.style.overflowY = '';
+    }
+});
+
+
+// popup par
